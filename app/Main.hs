6ec01963605dfb62fbdf1f63dc79a84ce525508e
Matteo Morena <8678043+xmamo@users.noreply.github.com>
2021-06-03 18:09:32 +0200

    Environment: set "LC_ALL" to "C"

--- app/Main.hs
+++ app/Main.hs
@@ -1,3 +1,11 @@
 module Main (main) where
 
-import "devin" Main (main)
+import qualified GI.GLib as GLib
+
+import qualified "devin" Main as Devin
+
+
+main :: IO ()
+main = do
+  GLib.setenv "LC_ALL" "C" True
+  Devin.main
