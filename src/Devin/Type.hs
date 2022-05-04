7eec004a9ffc2a8bb6d0e3351273bd9407b6f85d
Matteo Morena <8678043+xmamo@users.noreply.github.com>
2022-05-04 22:39:45 +0200

    Remove unneeded Function type

--- src/Devin/Type.hs
+++ src/Devin/Type.hs
@@ -7,7 +7,6 @@ module Devin.Type (
 ) where
 
 import Data.Data
-import Data.Functor.Classes
 
 import Devin.Display
 
@@ -20,7 +19,6 @@ data Type
   | Int
   | Float
   | Array Type
-  | Function [Type] Type
   | Placeholder String
   deriving (Show, Read, Data)
 
@@ -34,7 +32,6 @@ Bool <: Bool = True
 Int <: Int = True
 Float <: Float = True
 Array t1 <: Array t2 = t1 <: t2
-Function ts1 r1 <: Function ts2 r2 = liftEq (<:) ts1 ts2 && r1 <: r2
 Placeholder name1 <: Placeholder name2 = name1 == name2
 _ <: _ = False
 
@@ -49,12 +46,3 @@ instance Display Type where
   displays Float = showString "Float"
   displays (Array t) = showChar '[' . displays t . showChar ']'
   displays (Placeholder name) = showString name
-
-  displays (Function [] returnType) =
-    showString "() → " . displays returnType
-
-  displays (Function (parameterT : parameterTs) returnType) =
-    showChar '(' . displays parameterT . go parameterTs
-    where
-      go [] = showString ") → " . displays returnType
-      go (parameterT : parameterTs) = showString ", " . displays parameterT . go parameterTs
